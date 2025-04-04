e6476439e7eea4e34b462e4daf7a860a94381ea7
icyethics <97385843+icyethics@users.noreply.github.com>
2025-04-04 18:20:17 +0200

    more set up

--- Playsets.lua
+++ Playsets.lua
@@ -10,6 +10,26 @@ Game.init_game_object = function(self)
 
     if G.GAME.playsets then
         -- add cards to playset
+        if _ch.restrictions.banned_cards then
+            for k, v in ipairs(_ch.restrictions.banned_cards) do
+                G.GAME.banned_keys[v.id] = true
+                if v.ids then
+                    for kk, vv in ipairs(v.ids) do
+                        G.GAME.banned_keys[vv] = true
+                    end
+                end
+            end
+        end
+        if _ch.restrictions.banned_tags then
+            for k, v in ipairs(_ch.restrictions.banned_tags) do
+                G.GAME.banned_keys[v.id] = true
+            end
+        end
+        if _ch.restrictions.banned_other then
+            for k, v in ipairs(_ch.restrictions.banned_other) do
+                G.GAME.banned_keys[v.id] = true
+            end
+        end
     end
 
     return ret
---
+++ items/playsets/playset_object.lua
@@ -0,0 +1,39 @@
+return {
+
+    -- The additional info about your playset
+    metadata = {
+        -- Inclusive set to true if you want the elements in the list to be added to the run. Exclusive removes them
+        inclusive = true,
+
+        -- the IDs of the required mods. The card set will only load if each of these mods has been loaded
+        required_mods = {
+
+        }
+    },
+
+    cards = {
+        -- Jokers (Add the full key here, as a string, to add it to the playset)
+        -- j_jimbo,
+
+        -- Consumables
+
+        -- Boosters
+
+        -- Enhancements
+
+        -- Editions
+
+        -- Vouchers
+    },
+    tags = {
+        
+
+    },
+    other = {
+        -- Objects banned in the other category need not only their id but their 
+        
+        -- Blinds
+
+
+    },
+}
